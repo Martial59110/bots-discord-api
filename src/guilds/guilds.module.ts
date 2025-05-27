420d9b5957807e4eac3c46e09afd541f4bd8b837
Martial59110 <floquet.martial@gmail.com>
2025-05-28 01:45:57 +0200

    fix: import DiscordBotModule dans GuildsModule

--- src/guilds/guilds.module.ts
+++ src/guilds/guilds.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GuildsService } from './guilds.service';
 import { GuildsController } from './guilds.controller';
 import { Guild } from './entities/guild.entity';
+import { DiscordBotModule } from '../discord-bot/discord-bot.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Guild])],
+  imports: [
+    TypeOrmModule.forFeature([Guild]),
+    DiscordBotModule
+  ],
   controllers: [GuildsController],
   providers: [GuildsService],
   exports: [GuildsService]
