ef5ddd6d35d19230c3e86b32ee16fbadc510b48b
Martial59110 <floquet.martial@gmail.com>
2025-05-30 20:42:09 +0200

    chore(members): update module imports and providers

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -4,9 +4,17 @@ import { MembersController } from './members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
 import { RolesModule } from 'src/roles/roles.module';
+import { Role } from '../roles/entities/role.entity';
+import { DiscordBotModule } from '../discord-bot/discord-bot.module';
+import { DiscordModule } from '../discord/discord.module';
+import { DiscordUser } from '../discord-users/entities/discord-user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member]), RolesModule],
+  imports: [
+    TypeOrmModule.forFeature([Member, Role, DiscordUser]),
+    DiscordModule,
+    DiscordBotModule
+  ],
   controllers: [MembersController],
   providers: [MembersService],
   exports: [MembersService]
