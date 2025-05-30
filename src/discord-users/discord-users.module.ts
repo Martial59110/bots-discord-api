52107fad38293ac53633edc67154c007e2352f7f
Martial59110 <floquet.martial@gmail.com>
2025-05-30 20:42:09 +0200

    chore(discord-users): update module imports and providers

--- src/discord-users/discord-users.module.ts
+++ src/discord-users/discord-users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiscordUsersService } from './discord-users.service';
 import { DiscordUsersController } from './discord-users.controller';
 import { DiscordUser } from './entities/discord-user.entity';
+import { Member } from '../members/entities/member.entity';
 import { DiscordBotModule } from '../discord-bot/discord-bot.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DiscordUser]), DiscordBotModule],
+  imports: [TypeOrmModule.forFeature([DiscordUser, Member]), DiscordBotModule],
   controllers: [DiscordUsersController],
   providers: [DiscordUsersService],
   exports: [DiscordUsersService]
