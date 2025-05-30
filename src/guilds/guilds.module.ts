e7b76a5dd0a509fc780f460623a9a933c98bf648
Martial59110 <floquet.martial@gmail.com>
2025-05-30 20:42:09 +0200

    chore(guilds): update module imports and providers

--- src/guilds/guilds.module.ts
+++ src/guilds/guilds.module.ts
@@ -4,11 +4,15 @@ import { GuildsService } from './guilds.service';
 import { GuildsController } from './guilds.controller';
 import { Guild } from './entities/guild.entity';
 import { DiscordBotModule } from '../discord-bot/discord-bot.module';
+import { FormationsModule } from '../formations/formations.module';
+import { Member } from '../members/entities/member.entity';
+import { Promotion } from '../promotions/entities/promotion.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Guild]),
-    DiscordBotModule
+    TypeOrmModule.forFeature([Guild, Member, Promotion]),
+    DiscordBotModule,
+    FormationsModule
   ],
   controllers: [GuildsController],
   providers: [GuildsService],
