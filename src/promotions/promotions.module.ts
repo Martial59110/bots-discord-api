3ee4fcdb218e3cd8a9f767e2e8c27dda4b92aa95
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:19:26 +0200

    feat(promotions): add MembersModule import for automatic member creation

--- src/promotions/promotions.module.ts
+++ src/promotions/promotions.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PromotionsService } from './promotions.service';
+import { PromotionLifecycleService } from './promotion-lifecycle.service';
 import { PromotionsController } from './promotions.controller';
 import { Promotion } from './entities/promotion.entity';
 import { Role } from '../roles/entities/role.entity';
@@ -9,15 +10,25 @@ import { Category } from '../categories/entities/category.entity';
 import { FormationsModule } from '../formations/formations.module';
 import { DiscordBotModule } from '../discord-bot/discord-bot.module';
 import { PromotionsBotService } from './promotions-bot.service';
+import { CampusesModule } from '../campuses/campuses.module';
+import { ChannelsModule } from '../channels/channels.module';
+import { RolesModule } from '../roles/roles.module';
+import { AuthModule } from '../auth/auth.module';
+import { MembersModule } from '../members/members.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Promotion, Role, Member, Category]),
     FormationsModule,
-    DiscordBotModule
+    DiscordBotModule,
+    CampusesModule,
+    ChannelsModule,
+    RolesModule,
+    AuthModule,
+    MembersModule,
   ],
   controllers: [PromotionsController],
-  providers: [PromotionsService, PromotionsBotService],
-  exports: [PromotionsService]
+  providers: [PromotionsService, PromotionLifecycleService, PromotionsBotService],
+  exports: [PromotionsService, PromotionLifecycleService]
 })
 export class PromotionsModule {} 
