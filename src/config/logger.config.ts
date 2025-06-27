5a7a640b663d9ce4022f2ac152303f827bdf2c8e
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:20:21 +0200

    refactor(config): update logger configuration

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,14 +1,14 @@
 import { Params } from 'nestjs-pino';
+import { loggerConfig as prodConfig } from './logger.prod.config';
+import { loggerConfig as devConfig } from './logger.dev.config';
 
 let loggerConfig: Params;
 
 if (process.env.NODE_ENV === 'production') {
   // Charger la configuration de production
-  const { loggerConfig: prodConfig } = require('./logger.prod.config');
   loggerConfig = prodConfig;
 } else {
   // Charger la configuration de développement
-  const { loggerConfig: devConfig } = require('./logger.dev.config');
   loggerConfig = devConfig;
 }
 
