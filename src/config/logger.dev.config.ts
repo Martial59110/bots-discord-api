2af1671a0fcfa7155d9c32158e10846143185e6e
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:20:21 +0200

    refactor(config): update development logger configuration

--- src/config/logger.dev.config.ts
+++ src/config/logger.dev.config.ts
@@ -1,13 +1,14 @@
 import { Params } from 'nestjs-pino';
 
-export const loggerConfig = {
-  level: 'debug',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-      ignore: 'pid,hostname',
+export const loggerConfig: Params = {
+  pinoHttp: {
+    level: 'debug',
+    transport: {
+      target: 'pino/file',
+      options: {
+        destination: './logs/app.log',
+        mkdir: true
+      }
     },
   },
 }; 
