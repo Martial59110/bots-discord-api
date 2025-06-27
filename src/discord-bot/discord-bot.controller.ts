d01d8bad0cca3adc2dec2f1b68de79703b4f77d2
Martial59110 <floquet.martial@gmail.com>
2025-06-27 12:20:21 +0200

    refactor(discord-bot): update discord bot controller

--- src/discord-bot/discord-bot.controller.ts
+++ src/discord-bot/discord-bot.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { DiscordBotService } from './discord-bot.service';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('discord-bot')
 export class DiscordBotController {
   constructor(private readonly discordBotService: DiscordBotService) {}
 
   @Get('status')
+  @Public()
   getBotStatus() {
     const client = this.discordBotService.getClient();
     return {
