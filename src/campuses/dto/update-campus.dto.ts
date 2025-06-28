0d26ca5e62f32b517ae08b9a7dc9ee30e2c8bee7
Martial59110 <floquet.martial@gmail.com>
2025-06-28 22:05:42 +0200

    docs(campuses): add human documentation to update campus DTO

--- src/campuses/dto/update-campus.dto.ts
+++ src/campuses/dto/update-campus.dto.ts
@@ -1,3 +1,12 @@
+/* 
+ * DTO pour la mise à jour d'un campus existant
+ * 
+ * Ce DTO hérite de CreateCampusDto mais rend tous les champs optionnels
+ * grâce à PartialType. Ça permet de mettre à jour seulement certains champs
+ * sans avoir à fournir toutes les données.
+ * 
+ * Exemple : on peut changer juste le nom sans toucher au serveur Discord
+ */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCampusDto } from './create-campus.dto';
 
